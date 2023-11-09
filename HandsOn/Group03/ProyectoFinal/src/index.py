fd82a9f4556870c92bb30a5526172908e333fdc6
victorcamposs220 <vkmposs@outlook.com>
2023-11-09 19:34:21 +0100

    Añadido segundo ciclo de la aplicacion

--- HandsOn/Group03/ProyectoFinal/src/index.py
+++ HandsOn/Group03/ProyectoFinal/src/index.py
@@ -128,15 +128,50 @@ def districts_request():
         datos = {'resultados': string}
         return render_template('districts.html', data=datos)
 
-@app.route('/Companys')
+@app.route('/Companies')
 def companys():
-    return render_template('companys.html')
+    return render_template('companys.html', data="")
 
+@app.route('/Companies/Request')
+def companys_request():
+      if request.method == 'GET':
+        calle = request.args.get('street')
+        calle = str(calle)
+        q = prepareQuery('''
+            PREFIX ns: <http://smartcity.linkeddata.es/lcc/ontology/BicicletasElectricas#> 
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>   
+            PREFIX data: <http://smartcity.linkeddata.es/lcc/resource/>  
+            PREFIX bicycle: <data:BicycleStation> 
+            PREFIX district: <data:District> 
+            PREFIX direction: <data:Direction>  
+            PREFIX hood: <data:Neighbourhood>  
+            PREFIX street: <data:Street>           
+
+            SELECT DISTINCT ?Company WHERE {
+                ?Street rdfs:label "''' + calle + '''". 
+                ?Direction ns:hasStreet ?Street.                
+                ?BicycleStation ns:hasDirection ?Direction.
+                ?BicycleStation ns:isManageBy ?Company.
+            } LIMIT 10'''
+        )
+
+        ##ADAPTER##
+        string = []
+        for r in g.query(q):
+            string.append(r.Company)
+
+        if len(string) == 0:
+            string.append("Dicha calle no alberga ninguna bases de bicicletas")
+
+        datos = {'resultados': string}
+        return render_template('companys.html', data=datos)
+        
 ## CONSULTAS LINKEADAS ##
 
 @app.route('/StreetsLinked')
 def streetsLinked():
-    return render_template('streets-linked.html', data="")
+    return render_template('streets-linked.html', data={})
 
 @app.route('/StreetsLinked/Request')
 def streetsLinked_request():
@@ -163,31 +198,142 @@ def streetsLinked_request():
 
 
         ##ADAPTER##
+        valores = {}
         clave = ''
         for r in g.query(q):
             clave= r.Key.split('/')
 
+        if clave != '':
         #DESCARGAMOS LA PAGINA DE WIKIDATA QUE NOS INTERESA
-        grafo = Graph()
-        grafo.parse("https://www.wikidata.org/wiki/Special:EntityData/"+clave[-1]+".ttl")
+            grafo = Graph()
+            grafo.parse("https://www.wikidata.org/wiki/Special:EntityData/"+clave[-1]+".ttl")
 
-        #CONSULTAMOS A ESA PAGINA DESCARGADA, CON prepareQuery ESTO NO FUNCIONA
-        q = grafo.query("""SELECT ?p ?label_objeto
-                    WHERE 
-                    {
-                        wd:"""+clave[-1]+""" ?p ?objeto.
-                        ?objeto rdfs:label ?label_objeto.
-                    }LIMIT 5""")
-        
-        #HACEMOS UN ADAPTER PARA RETENER LOS VALORES
-        valores = {}
-        for p, label_objeto in q:
-            valores[p] = label_objeto
+            #CONSULTAMOS A ESA PAGINA DESCARGADA, CON prepareQuery ESTO NO FUNCIONA
+            q = grafo.query("""SELECT ?p ?label_objeto
+                        WHERE 
+                        {
+                            wd:"""+clave[-1]+""" ?p ?objeto.
+                            ?objeto rdfs:label ?label_objeto.
+                        }LIMIT 5""")
+            
+            #HACEMOS UN ADAPTER PARA RETENER LOS VALORES
+            for p, label_objeto in q:
+                valores[p] = label_objeto
 
-        if len(valores) == 0:
+        else:
             valores['resultado'] = "No tiene ningun link con datos de WIKIDATA"
 
         return render_template('streets-linked.html', data=valores)
+    
+
+@app.route('/NeighbourhoodLinked')
+def neighbourhoodLinked():
+    return render_template('neighbourhood-linked.html',data={})
+
+@app.route('/NeighbourhoodLinked/Request')
+def neighbourhoodLinked_request():
+    if request.method == 'GET':
+        hood = request.args.get('hood')
+        hood = str(hood)
+        q = prepareQuery('''
+            PREFIX ns: <http://smartcity.linkeddata.es/lcc/ontology/BicicletasElectricas#> 
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>   
+            PREFIX data: <http://smartcity.linkeddata.es/lcc/resource/>  
+            PREFIX bicycle: <data:BicycleStation> 
+            PREFIX district: <data:District> 
+            PREFIX direction: <data:Direction>  
+            PREFIX hood: <data:Neighbourhood>  
+            PREFIX street: <data:Street>           
+
+            SELECT ?Key WHERE {
+                ?Neighbourhood rdfs:label "''' + hood + '''". 
+                ?Neighbourhood owl:sameAs ?Key
+            } LIMIT 1'''
+        ) 
+        
+        ##ADAPTER##
+        valores = {}
+        clave = ''
+        for r in g.query(q):
+            clave= r.Key.split('/')
+
+        if clave != '':
+            #DESCARGAMOS LA PAGINA DE WIKIDATA QUE NOS INTERESA
+            grafo = Graph()
+            grafo.parse("https://www.wikidata.org/wiki/Special:EntityData/"+clave[-1]+".ttl")
+
+            #CONSULTAMOS A ESA PAGINA DESCARGADA, CON prepareQuery ESTO NO FUNCIONA
+            q = grafo.query("""SELECT ?p ?label_objeto
+                        WHERE 
+                        {
+                            wd:"""+clave[-1]+""" ?p ?objeto.
+                            ?objeto rdfs:label ?label_objeto.
+                        }LIMIT 5""")
+            
+            #HACEMOS UN ADAPTER PARA RETENER LOS VALORES
+            for p, label_objeto in q:
+                valores[p] = label_objeto
+
+        else:
+            valores['resultado'] = "No tiene ningun link con datos de WIKIDATA"
+
+        return render_template('neighbourhood-linked.html', data=valores)
+
+@app.route('/DistrictLinked')
+def DistrictLinked():
+    return render_template('districts-linked.html', data={})
+
+@app.route('/DistrictLinked/Request')
+def DistricLinked_request():
+    if request.method == 'GET':
+        distrito = request.args.get('district')
+        distrito = str(distrito)
+        q = prepareQuery('''
+            PREFIX ns: <http://smartcity.linkeddata.es/lcc/ontology/BicicletasElectricas#> 
+            PREFIX xsd: <http://www.w3.org/2001/XMLSchema#> 
+            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>   
+            PREFIX data: <http://smartcity.linkeddata.es/lcc/resource/> 
+            PREFIX owl: <http://www.w3.org/2002/07/owl#>   
+            PREFIX bicycle: <data:BicycleStation> 
+            PREFIX district: <data:District> 
+            PREFIX direction: <data:Direction>  
+            PREFIX hood: <data:Neighbourhood>  
+            PREFIX street: <data:Street>           
+
+            SELECT ?Key WHERE {
+                ?Distric rdfs:label "''' + distrito + '''". 
+                ?Distric owl:sameAs ?Key
+            } LIMIT 1'''
+        )
+
+        ##ADAPTER##
+        valores = {}
+        clave = ''
+        for r in g.query(q):
+            clave= r.Key.split('/')
+
+        if clave != '':
+            #DESCARGAMOS LA PAGINA DE WIKIDATA QUE NOS INTERESA
+            grafo = Graph()
+            grafo.parse("https://www.wikidata.org/wiki/Special:EntityData/"+clave[-1]+".ttl")
+
+            #CONSULTAMOS A ESA PAGINA DESCARGADA, CON prepareQuery ESTO NO FUNCIONA
+            q = grafo.query("""SELECT ?p ?label_objeto
+                        WHERE 
+                        {
+                            wd:"""+clave[-1]+""" ?p ?objeto.
+                            ?objeto rdfs:label ?label_objeto.
+                        }LIMIT 5""")
+            
+            #HACEMOS UN ADAPTER PARA RETENER LOS VALORES
+            for p, label_objeto in q:
+                valores[p] = label_objeto
+
+        else:
+            valores['resultado'] = "No tiene ningun link con datos de WIKIDATA"
+
+        return render_template('districts-linked.html', data=valores)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)         ##Indicamos que cada vez que cambiemos algo  se reinicie el servidor, y en que puerto lanzarse
